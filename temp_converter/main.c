c9ca77644b0a5c19a10cedddb8a2437c33d28cea
xpan <xpanvictor@gmail.com>
2023-01-09 18:01:51 +0100

    Refr: Documentation using symbolic constants

--- temp_converter/main.c
+++ temp_converter/main.c
@@ -5,14 +5,13 @@ int main(){
 	float celsius;
 	int fahr, lower, upper, step;
 	
-	lower = 0;
-	upper = 300;
-
-	step = 20;
+	#define LOWER 0 /* the lower limit of the temperatures */
+	#define UPPER 300 /* the upper limit of the temperatures */
+	#define STEP 20 /* the intervals for the temp */
 
 	/* for loop going from lower to upper in steps */
 	
-	for (fahr = lower; fahr <= upper; fahr += step){
+	for (fahr = LOWER; fahr <= UPPER; fahr += STEP){
 		celsius = (5.0 / 9.0) * (fahr - 32);
 		printf("%3d %6.1f\n", fahr, celsius);	
 	}
