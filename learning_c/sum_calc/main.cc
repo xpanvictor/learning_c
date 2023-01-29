67c461696936b84dfde1452bf191aa8ee4a06c33
xpan <xpanvictor@gmail.com>
2023-01-29 11:15:40 +0100

    Feat: use scanf to get int input and run sum op

---
+++ learning_c/sum_calc/main.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(){
+
+	int a, b;
+
+	printf("Input a: ");
+	scanf("%d", &a);
+	printf("Input b: ");
+	scanf("%d", &b);
+
+	printf("The sum of %d and %d is %d\n", a, b, a + b);
+
+	return 0;
+}
