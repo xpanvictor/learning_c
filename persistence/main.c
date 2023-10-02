6439250ebb15e8830dd501e72376bdd504005e54
xpanvictor <xpanvictor@gmail.com>
2023-10-02 23:29:08 +0100

    feat(ls): small implementation of ls

---
+++ persistence/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <inttypes.h>
+
+int main(int argc, char *argv[])
+{
+
+}
---
+++ unix_structure/ls/main.c
@@ -0,0 +1,25 @@
+#include "apue.h"
+#include <dirent.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+  DIR *dir_handle;
+  struct dirent *dir_reader;
+
+  // check if dir to list content is valid
+  if (argc != 2)
+    fprintf(stderr, "Usage: ls <dir name>");
+
+  if ((dir_handle = opendir(argv[1])) == NULL)
+    printf("Could not open %s", argv[1]);
+
+  while ((dir_reader = readdir(dir_handle)) != NULL)
+    printf("%s\n", dir_reader->d_name);
+
+  // close the handle 
+  closedir(dir_handle);
+  exit(0);
+}
+
