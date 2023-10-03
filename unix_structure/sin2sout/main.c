c92f66fd80b12e57f638dfb9c3c9f22ad304fa38
xpanvictor <xpanvictor@gmail.com>
2023-10-03 22:02:38 +0100

    feat(file_descriptors): working with file descriptors

---
+++ unix_structure/sin2sout/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUFFSIZE 4096
+
+void err_sys(char *msg)
+{
+  printf(msg);
+  exit(1);
+}
+
+int main(void)
+{
+  int n;
+  char buf[BUFFSIZE];
+
+  while((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+    if (write(STDOUT_FILENO, buf, n) != n)
+      err_sys("Write error");
+
+  if (n < 0)
+    err_sys("Read error");
+
+  exit(0);
+}
+
